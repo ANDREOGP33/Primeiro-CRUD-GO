087ca91622392ae71a07f03dcf7055b0f2a00390
ANDREOGP33 <105257924+ANDREOGP33@users.noreply.github.com>
2024-02-19 23:04:22 -0300

    Validando campos da requisição - Meu Primeiro CRUD em GoLang | AULA #7

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -14,7 +14,7 @@ func CreateUser(c *gin.Context) {
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
 		restErr := rest_err.NewBadRequestError(
-			fmt.Sprintf("there are some incorrect filds, error=%s\n", err.Error))
+			fmt.Sprintf("there are some incorrect filds, error=%s", err.Error))
 
 		c.JSON(restErr.Code, restErr)
 		return
