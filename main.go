494cd9c0794e576f08c4a49f95c5c2d4bf13cd02
ANDREOGP33 <andregasparoni690@gmail.com>
2024-02-21 13:21:28 -0300

    Adicionando logs ao projeto - Meu Primeiro CRUD em GoLang | AULA #9

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"log"
 
+	"github.com/ANDREOGP33/Primeiro-CRUD-GO/tree/main/src/configuration/logger"
 	"github.com/ANDREOGP33/Primeiro-CRUD-GO/tree/main/src/controller/routes"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
 func main() {
+	logger.Info("about to start application")
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,33 +1,44 @@
 package controller
 
 import (
-	"fmt"
-	"log"
 	"net/http"
 
+	"github.com/ANDREOGP33/Primeiro-CRUD-GO/tree/main/src/configuration/logger"
 	"github.com/ANDREOGP33/Primeiro-CRUD-GO/tree/main/src/configuration/validation"
 	"github.com/ANDREOGP33/Primeiro-CRUD-GO/tree/main/src/controller/model/request"
 	"github.com/ANDREOGP33/Primeiro-CRUD-GO/tree/main/src/controller/model/response"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
 func CreateUser(c *gin.Context) {
-
+	logger.Info("Init createUser controller",
+		zap.Field{
+			Key:    "journey",
+			String: "createUser",
+		},
+	)
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
-		log.Printf("Error trying to marshal object, error=%s\n", err.Error())
+		logger.Error("Error trying to validate user info", err, zap.Field{
+			Key:    "journey",
+			String: "createUser",
+		})
 		errReset := validation.ValidateUserError(err)
 
 		c.JSON(errReset.Code, errReset)
 		return
 	}
-	fmt.Println(userRequest)
 	response := response.UserResponse{
 		ID:    "test",
 		Email: userRequest.Email,
 		Name:  userRequest.Name,
 		Age:   userRequest.Age,
 	}
+	logger.Info("User created successfully", zap.Field{
+		Key:    "journey",
+		String: "createUser",
+	})
 	c.JSON(http.StatusOK, response)
 }
